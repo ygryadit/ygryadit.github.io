d94dff134b36f5d19e23061d3961f0a55f66db82
yulia <yulia.gryaditskay@inria.fr>
2022-09-21 16:35:37 +0100

    adding links and change the email address

--- myaddress.js
+++ myaddress.js
@@ -1,4 +1,4 @@
-var user_name = "yulia.gryaditskaya";
-var host_name = "gmail.com"
+var user_name = "y.gryaditskaya";
+var host_name = "surrey.ac.uk"
 var link = user_name + "@" + host_name;
 document.write("<a href=\"" + "mai" + "lto:" + user_name + "@" + host_name + "\">" + link + "</a>");
